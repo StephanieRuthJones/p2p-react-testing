d794599c0f7be24230bca1be630b8bfc4600d31d
Stephanie Jones <stephanieruthjones@gmail.com>
2022-05-13 09:39:03 -0600

    Add button

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 import { useState, useEffect } from 'react'
 import './App.css'
-import { Loading } from './components/loading/Loading'
 import PostsContainer from './components/postsContainer/PostsContainer.jsx'
 
 function App() {
   const [posts, setPosts] = useState([])
-  const [isError, setIsError] = useState(false)
-  const [isLoading, setIsLoading] = useState(false)
 
   const fetchPosts = async () => {
-    setIsError(false)
-    setIsLoading(true)
     try {
         const response = await fetch('https://jsonplaceholder.typicode.com/posts')
         const postData = await response.json()
@@ -19,18 +14,13 @@ function App() {
     catch (error) {
         console.error('error', error)
     }
-    setIsLoading(false)
   }
-
-  useEffect(() => {
-    fetchPosts()
-  }, [])
 
   return (
     <div className="App">
       <h1>Peer to Peer React Testing Library</h1>
-      {isLoading
-        ? <Loading />
+      {!posts?.length 
+        ? <button onClick={fetchPosts}>Get Posts</button>
         : <PostsContainer posts={posts} />}
     </div>
   );
